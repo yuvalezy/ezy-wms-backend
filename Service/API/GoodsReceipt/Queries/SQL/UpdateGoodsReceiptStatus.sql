92c6173cdc2c57f1a9b404e2cf6f1528c20e87f7
yuvallerner <yuval.lerner@beonesolutions.com>
2023-09-24 09:57:06 -0500

    Fixed target status

--- Service/API/GoodsReceipt/Queries/SQL/UpdateGoodsReceiptStatus.sql
+++ Service/API/GoodsReceipt/Queries/SQL/UpdateGoodsReceiptStatus.sql
@@ -1,4 +1,1 @@
 ﻿update [@LW_YUVAL08_GRPO] set "U_Status" = @Status, "U_StatusDate" = getdate(), "U_StatusEmpID" = @empID where "Code" = @ID;
-If @Status = 'C' Begin
-    update [@LW_YUVAL08_GRPO1] set U_TargetStatus = 'C' where U_ID = @ID
-end
