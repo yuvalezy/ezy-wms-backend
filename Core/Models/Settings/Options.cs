15aee52c2fcc35b9baa8b4b0ca22ff02da572a19
Yuval Lerner <yuval@ezyprop.com>
2025-07-23 08:37:19 -0500

    ,     "GoodsReceiptPackages": true

--- Core/Models/Settings/Options.cs
+++ Core/Models/Settings/Options.cs
@@ -2,21 +2,29 @@ using Core.Enums;
 
 namespace Core.Models.Settings;
 
-public record Options {
-    public bool                     GoodsReceiptDraft                           { get; init; }
-    public bool                     GoodsReceiptModificationsRequiredSupervisor { get; init; }
-    public bool                     GoodsReceiptCreateSupervisorRequired        { get; init; }
-    public bool                     GoodsReceiptTargetDocuments                 { get; set; }
-    public bool                     TransferTargetItems                         { get; init; }
-    public bool                     EnablePackages                              { get; init; }
-    public GoodsReceiptDocumentType GoodsReceiptType                            { get; set; } = GoodsReceiptDocumentType.Both;
-    public UnitType                 DefaultUnitType                             { get; set; } = UnitType.Pack;
-    public bool                     EnableUnitSelection                         { get; init; }
+public record Options
+{
+    // Goods Receipt
+    public bool GoodsReceiptDraft { get; init; }
+    public bool GoodsReceiptModificationsRequiredSupervisor { get; init; }
+    public bool GoodsReceiptCreateSupervisorRequired { get; init; }
+    public GoodsReceiptDocumentType GoodsReceiptType { get; set; } = GoodsReceiptDocumentType.Both;
+    public bool GoodsReceiptTargetDocuments { get; set; }
+    public bool GoodsReceiptPackages { get; set; } = true;
+
+    //Transfer
+    public bool TransferTargetItems { get; init; }
+
+    //Packages & Units
+    public bool EnablePackages { get; init; }
+    public UnitType DefaultUnitType { get; set; } = UnitType.Pack;
+    public bool EnableUnitSelection { get; init; }
 
     /// <summary>
     /// Idle Settings for auto log out, if null or zero, ignore
     /// </summary>
     public int? IdleLogoutTimeout { get; set; }
 
+    //Pick List
     public bool EnablePickingCheck { get; set; }
 }
